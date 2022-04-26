c0af7512698d3cbcde52a2f5333613e43fe15295
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-04-26 13:59:30 -0500

    fix mistake in hot_wheel

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+hot_wheel = {"model" => "mustang cobra", "color" => "black", "price" => 7.99}
+soccer_ball = {:color => "red", :price => 9.99}
+baseball_bat = {color: "yellow", price: 35.99, edition: "world series champion"}
+
+puts hot_wheel["model"]
