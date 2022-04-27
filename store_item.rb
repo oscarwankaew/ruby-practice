e19c6fce404fab1b85be39cc5f8d63d39712cec3
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-04-27 11:33:15 -0500

    implement attr

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,42 @@
-hot_wheel = {"model" => "mustang cobra", "color" => "black", "price" => 7.99}
-soccer_ball = {:color => "red", :price => 9.99}
-baseball_bat = {color: "yellow", price: 35.99, edition: "world series champion"}
+item1 = {"type" => "Hot wheel", "color" => "black", "price" => 7.99}
+item2 = {:type => "soccer ball", :color => "red", :price => 9.99}
+item3 = {typt: "Baseball bat", color: "yellow", price: 35.99}
 
-puts hot_wheel["model"]
+puts "#{item1["type"]} with color #{item1["color"]} costs #{item1["price"]}"
+
+
+class Items
+    attr_accessor :item, :color, :price
+
+    def initialize(input_item, input_color, input_price)
+        @item = input_item
+        @color = input_color
+        @price = input_price
+    end
+    def info
+        puts "#{item} has color #{color} and it costs #{price}."
+    end
+    def item
+        return @item
+    end
+    def color 
+        return @color
+    end
+    def price
+        return @price
+    end
+end
+
+item1 = Items.new("Hot wheel", "Black", 7.99)
+item2 = Items.new("Soccer ball", "Red", 9.99)
+item3 = Items.new("Baseball bat", "Yellow", 35.99)
+item1.info
+item2.info
+item3.info
+p item1.item
+p item2.color
+p item3.price
+item3.item = "Shoes"
+p item3
+item1.color = "White"
+p item1
