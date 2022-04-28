cdf94ce5eaafd7b30206800113fc9272c4f9fc6e
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-04-28 12:06:17 -0500

    Implemented module

--- store_item.rb
+++ store_item.rb
@@ -1,39 +1,60 @@
-item1 = {"type" => "Hot wheel", "color" => "black", "price" => 7.99}
-item2 = {:type => "soccer ball", :color => "red", :price => 9.99}
-item3 = {typt: "Baseball bat", color: "yellow", price: 35.99}
+module Itemidable
 
-puts "#{item1["type"]} with color #{item1["color"]} costs #{item1["price"]}"
-
-
-class Items
-    attr_accessor :name, :color, :price
+    attr_writer :name, :color   
 
     def initialize(input_options)
         @name = input_options[:name]
         @color = input_options[:color]
         @price = input_options[:price]
     end
-    def info
-        puts "#{name} has color #{color} and it costs #{price}."
-    end
+
     def name
         return @name
     end
+
     def color 
         return @color
     end
+
     def price
         return @price
     end
+
     def tax
         return @price * 1.04
     end
 
 end
 
-item1 = Items.new({name: "Hot wheel", color: "Black", price: 7.99})
-item2 = Items.new({name: "Soccer ball", color: "Red", price: 9.99})
-item3 = Items.new({name: "Baseball bat", color: "Yellow", price: 35.99})
+module Perishable
+
+    def initialize(input_options)
+        super
+      @shelf_life = input_options[:shelf_life]
+    end
+    def shelf_life
+        return @shelf_life
+    end
+    def info
+        puts "#{name} is #{price} and it expires #{shelf_life}"
+    end
+ 
+end
+ 
+class Item
+
+    include Itemidable
+    
+    def info
+        puts "#{name} has color #{color} and it costs #{price}."
+    end
+
+
+end
+
+item1 = Item.new({name: "Hot wheel", color: "Black", price: 7.99})
+item2 = Item.new({name: "Soccer ball", color: "Red", price: 9.99})
+item3 = Item.new({name: "Baseball bat", color: "Yellow", price: 35.99})
 item1.info
 item2.info
 item3.info
@@ -46,19 +67,10 @@ item1.color = "White"
 p item1
 p item1.tax
 
-class Food < Items
+class Food < Item
 
-    def initialize(input_options)
-        super
-      @shelf_life = input_options[:shelf_life]
-    end
-    def shelf_life
-        return @shelf_life
-    end
-    def info
-        puts "#{name} is #{price} and it expires #{shelf_life}"
-    end
-
+    include Perishable
+    include Itemidable
 end
 
 edible1 = Food.new({name: "Bannana", price: 1.59, shelf_life: "3 days"})
@@ -66,25 +78,3 @@ edible2 = Food.new({name: "Milk", price: 1.59, shelf_life: "3 days"})
 edible1.info
 edible2.info
 
-puts "[C]reate [R]ead [U]date [D]elete [Q]uit: "
-input = gets.chomp
-if input == d
-    puts "Delete item"
-    puts "Item id: "
-    item_id = gets.chomp
-elsif input == r
-    puts "Item id: "
-    item_id = gets.chomp
-elsif input == c
-    puts "Item name: "
-    name = gets.chomp
-    puts "Item color: "
-    color = gets.chomp
-    puts "Item price: "
-    price = gets.chomp
-elsif input == u
-    puts "Item id: "
-    item_id = gets.chomp
-    puts "Update item expiration: "
-elsif input == q
-end    
